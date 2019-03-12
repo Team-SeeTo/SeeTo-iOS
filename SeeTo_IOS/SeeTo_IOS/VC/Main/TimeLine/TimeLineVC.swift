a6894ae0849d8d9b55b0f24cb4946ff8e8d62091
Woopa01 <whdnwls49@naver.com>
2019-03-12 09:49:59 +0900

    add timeline

--- SeeTo_IOS/SeeTo_IOS/VC/Main/TimeLine/TimeLineVC.swift
+++ SeeTo_IOS/SeeTo_IOS/VC/Main/TimeLine/TimeLineVC.swift
@@ -11,10 +11,38 @@ import UIKit
 import XLPagerTabStrip
 
 class TimeLineVC : UIViewController , IndicatorInfoProvider {
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
-        
+        tableview.dataSource = self
+        tableview.delegate = self
     }
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "TimeLine")
+    }
+}
+
+extension TimeLineVC : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "TLCell", for: indexPath as IndexPath) as! TimeLineCell
+        
+        cell.FirstNumber.text = "13"
+        cell.SecondNumber.text = "7"
+        cell.ThirdNumber.text = "2"
+        cell.TotalPoint.text = "230p"
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
     }
 }
