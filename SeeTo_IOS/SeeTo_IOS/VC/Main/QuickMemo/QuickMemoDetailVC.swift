cbe35ad6d89a5101008881479fbfd424525139e2
Woopa01 <whdnwls49@naver.com>
2019-03-12 09:50:14 +0900

    add QuickMemo

---
+++ SeeTo_IOS/SeeTo_IOS/VC/Main/QuickMemo/QuickMemoDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  QuickMemoDetailVC.swift
+//  SeeTo_IOS
+//
+//  Created by 조우진 on 11/03/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuickMemoDetailVC : UIViewController {
+    @IBOutlet weak var QuickMemoTV: UITextView!
+    
+    override func viewDidLoad() {
+        
+    }
+}
+
--- SeeTo_IOS/SeeTo_IOS/VC/Main/QuickMemo/QuickMemoVC.swift
+++ SeeTo_IOS/SeeTo_IOS/VC/Main/QuickMemo/QuickMemoVC.swift
@@ -11,11 +11,41 @@ import UIKit
 import XLPagerTabStrip
 
 class QuickMemoVC : UIViewController , IndicatorInfoProvider{
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
-        
+        tableview.dataSource = self
+        tableview.delegate = self
     }
     
-    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
-        return IndicatorInfo(title: "QuickMemo")
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo { return IndicatorInfo(title: "QuickMemo") }
+}
+
+extension QuickMemoVC : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "QMCell", for: indexPath as IndexPath) as! QuickMemoCell
+        
+        cell.PreviewText.text = "hi hello"
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let QuickMemoDetail = self.storyboard?.instantiateViewController(withIdentifier: "QuickMemoDetail")
+        
+        self.navigationController?.pushViewController(QuickMemoDetail!, animated: true)
+    }
+    
 }
